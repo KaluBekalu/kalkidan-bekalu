f45ef23a05f29159ac677c11fdb985234c39b98d
kalubekalu <kalubekalu1@gmail.com>
2023-03-20 11:27:21 +0300

    refactoring

---
+++ src/content_option.js
@@ -0,0 +1,195 @@
+const logotext = "Kal";
+const meta = {
+  title: "Kalkidan Bekalu",
+  description:
+    "I’m Kalkidan Bekalu -  Full stack web devloper, currently located in Ethiopia 🇪🇹",
+};
+
+const introdata = {
+  title: "I’m Kalkidan Bekalu",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Skilled Full Stack Developer, Delivering Exceptional and Efficient Web Solutions.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "A bit about my self",
+  aboutme:
+    "I'm a Full Stack Developer with expertise in using cutting-edge technologies such as Node.js, React, and TypeScript, along with frameworks like Express. My passion for creating exceptional web solutions drives me to stay up-to-date with the latest trends in the industry. With a strong focus on delivering high-quality code and outstanding user experiences, I strive to exceed client expectations and take pride in every project I work on.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Frontend developer",
+    where: "Maraki",
+    date: "2019",
+  },
+  {
+    jobtitle: "Full stack developer",
+    where: "360Ground",
+    date: "2021",
+  },
+  {
+    jobtitle: "Freelancer",
+    where: "Upwork",
+    date: "2022",
+  },
+  {
+    jobtitle: "Full stack developer",
+    where: "Safaricom Ethiopia",
+    date: "2023",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Djano",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "Jquery",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Development",
+    description:
+      "From menus and buttons to forms and graphical components, I specialize in creating engaging and intuitive user interfaces that enhance the overall user experience. Using modern frontend technologies such as React and NextJs, I develop stunning user interfaces that are both responsive and highly usable.",
+  },
+  {
+    title: "Mobile Development",
+    description:
+      "I specialize in developing mobile apps that run seamlessly across platforms, including iOS and Android. Using React Native, I create apps that deliver an exceptional user experience and perform flawlessly on any device.",
+  },
+  {
+    title: "Fullstack Development",
+    description:
+      "With expertise in both frontend and backend development, I deliver complete end-to-end solutions that meet your specific needs. Using cutting-edge technologies such as Node.js and Express, I develop powerful backends that integrate seamlessly with engaging frontends developed with React and other modern technologies.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "pro.kidanbekalu@gmail.com",
+  YOUR_FONE: "(2519)46 393 208",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: process.env.SERVICE_ID,
+  YOUR_TEMPLATE_ID: process.env.TEMPLATE_ID,
+  YOUR_USER_ID: process.env.PUBLIC_KEY,
+};
+
+const socialprofils = {
+  github: "https://github.com/kalubekalu",
+  facebook: "https://facebook.com/fb.kalbekalu/",
+  linkedin: "https://linkedin.com/kalkidanbekalu",
+  twitter: "https://twitter.com",
+};
+
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
